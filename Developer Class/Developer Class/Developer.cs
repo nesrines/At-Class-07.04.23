33b7e8c245d66ba323129539734c1eb976b451c3
Nasrin F. Asadli <nasrinfa@code.local>
2023-04-07 17:31:48 +0400

    added Developer Class

---
+++ Developer Class/Developer Class/Developer.cs
@@ -0,0 +1,22 @@
+﻿ class Developer
+{
+    public string Name;
+    public string Surname;
+    public int Age;
+    public double Salary;
+    public string Position;
+
+    public void GetFullInfo()
+    {
+        Console.WriteLine(Name + " " + Surname + " " + Age + " " + Salary + " " + Position);
+    }
+
+    public Developer(string name, string surname, int age, int salary, string position)
+    {
+        Name = name;
+        Surname = surname;
+        Age = age;
+        Salary = salary;
+        Position = position;
+    }
+}
---
+++ Developer Class/Developer Class/Program.cs
@@ -0,0 +1,11 @@
+﻿Developer Developer1 = new Developer("Aysima", "Aslanova", 20, 2000, "Aaaaa");
+Developer Developer2 = new Developer("Bayram", "Bayramov", 25, 2500, "Bbbbb");
+Developer Developer3 = new Developer("Camila", "Camilova", 30, 3000, "Ccccc");
+Developer Developer4 = new Developer("Dadash", "Dadashov", 40, 4000, "Ddddd");
+Developer Developer5 = new Developer("Elnara", "Elnarova", 45, 4500, "Eeeee");
+
+Developer1.GetFullInfo();
+Developer2.GetFullInfo();
+Developer3.GetFullInfo();
+Developer4.GetFullInfo();
+Developer5.GetFullInfo();
